c7d65e23f56d0e2b78558a5ecfa61e0636f57d57
PCCLONBCN\Alumno <aa>
2022-10-20 17:52:07 +0200

    Project Updated
    Subido el proyecto

---
+++ Assets/Player/README.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class README : MonoBehaviour
+{
+
+    //SI TE SALE EN ROSA PONLE TU EL MATERIAL QUE YO NO LE HE PUESTO NINGUNO EN ESPECIFICO
+
+    //Si ves que solo salta una vez es porque a tu terreno has de ponerle un Layer que sea Ground   tal y como esta escrito y en el script de player movement que le pondras al player seleccionarlo
+
+    //SI QUIERES HACER WALL RUNNING HAS DE CREAR UN TAG QUE SE LLAME Wall 
+
+    //Te he puesto 2 paredes que se llaman Wall y Wall derecha, da igual la que pongas mientras esten en Layer Ground y tag Wall pero ponlas un poco inclinadas para que el jugador puede aferrarse a ellas, que no esten rectas
+
+    //Como yo lo he escrito sino no funcionara
+
+    //SUERTE
+
+
+
+
+
+}
