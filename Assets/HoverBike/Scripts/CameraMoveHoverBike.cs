8787da3781801f099e6aa60cd23a0acc3819c55b
PCCLONBCN\Alumno <edgarhipernovas@gmail.com>
2023-03-15 18:05:13 +0100

    Player

---
+++ Assets/HoverBike/Scripts/CameraMoveHoverBike.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveHoverBike : MonoBehaviour
+{
+    [SerializeField] float sensitivityX = 200f;
+    [SerializeField] float sensitivityY = 200f;
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    [SerializeField] float clampDerecha = -20f;
+    [SerializeField] float clampIzquierda = 40f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationX = Input.GetAxis("Mouse X") * sensitivityX * Time.deltaTime;
+
+        float rotationY = Input.GetAxis("Mouse Y") * sensitivityY * Time.deltaTime;
+
+        xRotation -= rotationY;
+        yRotation += rotationX;
+        xRotation = Mathf.Clamp(xRotation, -60, 90);
+        yRotation = Mathf.Clamp(yRotation, clampDerecha, clampIzquierda);
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation,0f);
+    }
+}
