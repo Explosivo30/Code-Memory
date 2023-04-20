414cffe1cdd101e665a77eaeb8922ad9c5b4ac27
PCCLONBCN\Alumno <edgarhipernovas@gmail.com>
2023-04-20 19:03:39 +0200

    Aldeano and enemy prefab

--- Assets/Aldeanos/AldeanoLookPlayer.cs
+++ Assets/Aldeanos/AldeanoLookPlayer.cs
@@ -6,8 +6,18 @@ public class AldeanoLookPlayer : MonoBehaviour
 {
     [SerializeField] AldeanoPlayerInside aldeanoPlayer;
 
-    //TODO Put ball between player and npc
+    [SerializeField] Transform player;
+    [SerializeField] Transform headAldeano;
+    [SerializeField] Transform headAimTracker;
 
+    Vector3 dirToPlayer;
 
+    private void FixedUpdate()
+    {
+        //if(player is inside)
+        //dirToPlayer = (player.position - headAldeano.position).normalized;
+        //Debug.DrawRay(transform.position, dirToPlayer);
+        headAimTracker.position = player.position;
+    }
 
 }
