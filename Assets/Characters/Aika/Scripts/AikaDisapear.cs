1516d853c8ef0d3a307991ed0818e61047b1f6e9
PCCLONBCN\Alumno <edgarhipernovas@gmail.com>
2023-05-23 17:16:11 +0200

    NewChanges

--- Assets/Characters/Aika/Scripts/AikaDisapear.cs
+++ Assets/Characters/Aika/Scripts/AikaDisapear.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class AikaDisapear : MonoBehaviour
 {
--- Assets/Scripts/Dialogos/DialogoNode.cs
+++ Assets/Scripts/Dialogos/DialogoNode.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEditor;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 namespace Dialogo
