53ca13fe6615d3e2d5f83d49626cd122ba3389d1
Explosivo30 <edgarhipernovas@gmail.com>
2023-05-30 12:55:34 +0200

    Conversations RootNode

---
+++ Assets/Scripts/Dialogos/PlayerConversant.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dialogo
+{
+    public class PlayerConversant : MonoBehaviour
+    {
+        [SerializeField] DialoguesAssetMenu currentDialogue;
+
+        public string GetText()
+        {
+            if(currentDialogue == null)
+            {
+                return "";
+            }
+
+           return currentDialogue.GetRootNode().GetDialogo();
+        }
+    }
+}
---
+++ Assets/Scripts/UI Dialogo/DialogueUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dialogo;
+using TMPro;
+
+namespace UI
+{
+    public class DialogueUI : MonoBehaviour
+    {
+        PlayerConversant playerConversant;
+        [SerializeField] TextMeshProUGUI AIText;
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
+            AIText.text = playerConversant.GetText();
+        }
+
+        
+
+    }
+}
+
